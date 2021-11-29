3e74b2a01ca1302da73109442a452c5457c2d930
ZHICHAO PENG <zhichaopeng@ZHICHAOs-MacBook-Air.local>
2021-11-29 14:03:43 -0500

    update the UQ

--- src/ExperimentAPI.jl
+++ src/ExperimentAPI.jl
@@ -19,6 +19,16 @@ function RotationFrameDiagonal(omega::Array{Float64},omega_drive::Float64)
     return HK
 end
 
+function RotationFrameDiagonal(omega,omega_drive)
+	diag_vec = 0.0
+	for i = 1:length(omega)
+		# potential troublemaker of auto-diff
+		diag_vec = [diag_vec;diag_vec[end]+omega[i]-omega_drive]
+	end
+	HK = Diagonal(diag_vec)
+    return HK
+end
+
 """
     RotationFrameDiagonal(gamma1::Array{Float64},gamma2::Array{Float64})
 
@@ -31,6 +41,18 @@ end
 - L_dephase: Diagonal{0,sqrt(gamma2[1]),...)
 """
 function RotationFrameLindblad(gamma1::Array{Float64},gamma2::Array{Float64})
+	#L_decay = Bidiagonal(zeros(length(gamma1)+1),sqrt.(gamma1),:U)
+	L_dephase = Diagonal([0;sqrt.(gamma2)])
+	L_decay = [zeros(Float64,length(gamma1)) Diagonal(sqrt.(gamma1))]
+	L_decay = [L_decay;zeros(Float64,1,length(gamma1)+1)]
+	#L_decay = [0.0 sqrt(gamma1[1]) 0.0 0.0;
+	#		   0.0 0.0      sqrt(gamma1[2]) 0.0;
+	#		   0.0 0.0      0.0      sqrt(gamma1[3]);
+	#		   0.0 0.0      0.0      0.0]
+    return L_decay, L_dephase
+end
+
+function RotationFrameLindblad(gamma1,gamma2)
 	#L_decay = Bidiagonal(zeros(length(gamma1)+1),sqrt.(gamma1),:U)
 	L_dephase = Diagonal([0;sqrt.(gamma2)])
 	L_decay = [zeros(Float64,length(gamma1)) Diagonal(sqrt.(gamma1))]
@@ -200,6 +222,132 @@ function RamseyForwardSolve(rho_u0::Array{Float64},rho_v0::Array{Float64},
 		end
 		# DiffEq interface for the controlled problem
 		controlled_problem = ODEProblem(ControlledODE!,rho_vec0,(0.0,0.5*TC),[1.0])
+		# Free propagation part
+		FreeEvolution = [ LD -LK_free;
+						  LK_free  LD]
+		# half-pi pusle
+		if(method=="DiffEqDefault")
+			sol = solve(controlled_problem;DiffEqKwargs...)
+		else
+			sol = solve(controlled_problem,method;DiffEqKwargs...)
+		end
+		rho_vec1 = sol[:,end]
+		# free propagation for a dark time
+		Free_operator = exp(t_dark_times[1]*FreeEvolution)
+		rho_vec2 = Free_operator*rho_vec1
+		# second half-pi pulse
+		controlled_problem = remake(controlled_problem,u0=rho_vec2)
+		if(method=="DiffEqDefault")
+			sol = solve(controlled_problem;DiffEqKwargs...)
+		else
+			sol = solve(controlled_problem,method;DiffEqKwargs...)
+		end
+		rho_ramsey = sol[:,end]
+		# free propagation for a dark time + second half-pi pulse
+		n_dark_times = length(t_dark_times)
+		for i = 2:n_dark_times
+			# free propagation
+			Free_operator = exp(t_dark_times[i]*FreeEvolution)
+			rho_vec2 = Free_operator*rho_vec1
+			# second half pi pulse
+			controlled_problem = remake(controlled_problem,u0=rho_vec2)
+			if(method=="DiffEqDefault")
+				sol = solve(controlled_problem;DiffEqKwargs...)
+			else
+				sol = solve(controlled_problem,method;DiffEqKwargs...)
+			end
+			rho_ramsey = [ rho_ramsey sol[:,end] ]
+		end
+	end
+	return rho_ramsey[1:N_states^2,:],rho_ramsey[N_states^2+1:end,:]
+end
+
+# Defined for the compatability of adjoint solves in Turing.jl to hold their data structure
+function RamseyForwardSolve(rho_u0,rho_v0,
+				   		  omega,omega_drive,
+				   		  gamma1,gamma2,
+						  InitialState,
+				   		  TC,t_dark_times,
+				   		  N_states::Int64=0;
+						  initial_type="states",
+						  method="exponential",
+						  DiffEqKwargs...)
+    if(N_states==0)
+		N_states = length(omega)+1
+	end
+	#################################
+	# Step 1: assemble operators
+	#################################
+	# Hamiltonian without control
+	HK_free = RotationFrameDiagonal(omega,omega_drive)
+	# Hamiltonain with control
+	if(method=="exponential")
+		Ω = 0.5*pi/(TC*sqrt(InitialState+1.0))
+		θ = 0.0
+		HK_control,HS_control = RotationFrameRamseyControl(Ω,θ,N_states)
+	else
+		HK_control,HS_control = RotationFrameRamseyControl(N_states)
+	end
+	# Lindblad operator
+	L1,L2 = RotationFrameLindblad(gamma1,gamma2)
+	# Assemble the operators for the vectorized system
+	# Half pi pulse
+	LK_half_pi,LS_half_pi,LD = make_lindblad_operator(Matrix(HK_control+HK_free),Matrix(HS_control),(L1,L2))
+	# Free evolution
+	LK_free = make_hamiltonian_operator(Matrix(HK_free))
+	#################################
+	# Step 2: Forward solves
+	#################################
+	# initial conditions
+	if(initial_type == "states")
+		rho_u_initial = (rho_u0*transpose(rho_u0)+rho_v0*transpose(rho_v0))[:]
+		rho_v_initial = (rho_v0*transpose(rho_u0)-rho_u0*transpose(rho_v0))[:]
+		rho_vec0 = [rho_u_initial;rho_v_initial]
+	elseif(initial_type == "density")
+		rho_vec0 = [rho_u0;rho_v0];
+	else
+		println("Error! initial_type must be \"density\" or \"states\"")
+		return
+	end
+	N_dark_times = length(t_dark_times)
+	if(method=="exponential")
+	#if(false)
+		# Ramsey experiment
+		Half_pi_operator = exp(0.5*TC*[LD+LS_half_pi -LK_half_pi; LK_half_pi LD+LS_half_pi])
+		FreeEvolution = [ LD -LK_free;
+					  	  LK_free  LD]
+		# half-pi pusle
+		rho_vec1 = Half_pi_operator*rho_vec0
+		# free propagation for a dark time
+		Free_operator = exp(t_dark_times[1]*FreeEvolution)
+		rho_vec2 = Free_operator*rho_vec1
+		# half-pi pulse
+		rho_ramsey = Half_pi_operator*rho_vec2
+		for i = 2:N_dark_times
+			# free propagation for a dark time
+			Free_operator = exp(t_dark_times[i]*FreeEvolution)
+			rho_vec2 = Free_operator*rho_vec1
+			# half-pi pulse
+			rho_ramsey = [rho_ramsey Half_pi_operator*rho_vec2]
+		end
+	else
+		function GaussianPulse(_time,_amplitude,_width)
+			σ = _width/sqrt(2.0*pi)
+			return _amplitude*exp(-(_time-0.5*TC)^2/(2.0*σ*σ))
+		end
+		width = TC/2.5
+		GaussianAmplitude = 0.5*pi/(width*sqrt(InitialState+1.0))
+		function ControlledODE!(_drho,_rho,_p,_t)
+			# Half pi pulse
+			_LK_half_pi,_LS_half_pi,_LD = make_lindblad_operator(HK_control*GaussianPulse(_t,GaussianAmplitude,width)+HK_free,
+															     HS_control*GaussianPulse(_t,GaussianAmplitude,width),
+															     (L1,L2))
+			SystemOperator  = [_LD+_LS_half_pi -_LK_half_pi;
+					  		   _LK_half_pi      _LD+_LS_half_pi]
+			_drho .= SystemOperator*_rho
+		end
+		# DiffEq interface for the controlled problem
+		controlled_problem = ODEProblem(ControlledODE!,rho_vec0,(0.0,0.5*TC),[omega;gamma1;gamma2])
 		# Free propagation part
 		FreeEvolution = [ LD -LK_free;
 						  LK_free  LD]
